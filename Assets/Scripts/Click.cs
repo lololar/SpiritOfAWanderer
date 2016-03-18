d572b8a091bcfb3fcbc3b1235e932bd4ebe2dc41
unknown <r.mathieu@J001-0140-15.etudiant.rubika-edu.com.local>
2016-03-18 09:49:37 +0100

    Ajout des assets principaux

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+    GameObject _object;
+
+	// Use this for initialization
+	void Start () {
+        _object = Resources.Load("Prefabs/Cube") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        /*if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("nsidngsdf");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                Instantiate(_object, hit.point, transform.rotation);
+        }*/
+
+	}
+}
